90bc0e77022d52f2c319ed1a90c232f2dcb03d08
Abduvohid123 <abduvoxid.mirzamaxmudov@mail.ru>
2025-09-16 17:43:12 +0500

    some changes

--- database.php
+++ database.php
@@ -1,1 +1,14 @@
 <?php
+
+$conn = mysqli_connect("localhost", "dragon", "password", "php_crud");
+
+if ($conn) {
+
+    $conn->query("INSERT INTO users (id,name, mobile_numer,created_at) VALUES (2,'Abdurahmon2','998991112234',now())");
+
+ $users=   $conn->query('select * from users')->fetch_all();
+ var_dump($users);
+
+}else{
+    echo "Connection Failed";
+}
